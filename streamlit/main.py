4f9339f5aed029ba16c5324312c863e6c340921f
julieta <julietaciare77@gmail.com>
2022-12-01 11:50:14 -0300

    prueba

--- streamlit/main.py
+++ streamlit/main.py
@@ -4,15 +4,7 @@ from streamlit_option_menu import option_menu
 from multiprocessing import Value
 from pages.main import home as m
 from pages.main import business as p
-<<<<<<< HEAD
 from pages.main import model as ml
-=======
-from PIL import Image
-
-##################
-## PAGE CONFIG ###
-##################
->>>>>>> a56658d55ed1db92a8945702d088064c12a49d91
 
 st.set_page_config(
    page_title="Vocado",
