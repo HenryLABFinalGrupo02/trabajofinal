25149f31a0dceb3a9568eef5bf5b933612842114
Maico <maico@MacBook-Pro-de-Maico.local>
2022-12-01 11:29:31 -0300

    ML deployed

--- streamlit/pages/main/model.py
+++ streamlit/pages/main/model.py
@@ -3,12 +3,16 @@ import pandas as pd
 import numpy as np
 from multiprocessing import Value
 from typing import List
-import joblib
+#import joblib
 #from prediction import predict
+import pickle
+import xgboost
 
-def predict(data):
-    clf = joblib.load('../../model/xgb_business.joblib')
-    return clf.predict(data)
+# def predict(data):
+#     #Refers to path of main.py
+#     #clf = joblib.load('./model/xgb_business.joblib')
+#     clf = pickle.load(open('./model/xgb_business.pkl', "rb"))
+#     return clf.predict(data)
 
 
 def machine_learning():
@@ -61,7 +65,7 @@ def machine_learning():
         "Open on weekends",
         help="Weekends mean friday, saturday and sundays")
 
-        open_hours = st.slider('Total open hours per day', min_value = 0, max_value = 24, value = 1)
+        open_hours = st.slider('Total open hours per day', min_value = 0.0, max_value = 24.0, value = 1.0)
 
 
     st.header("Additional features")
@@ -129,42 +133,148 @@ def machine_learning():
         "Wifi",
         help="Wifi")  
 
+        reservations = st.checkbox(
+        "Accept reservations",
+        help="Accept reservations")  
+
+        meal_diversity = st.slider('Meal diversity', min_value = 0, max_value = 6, value = 1,
+        help = "Meal diversity, 1 being only breakfast or dinner, 6 being all meals")
+
 
     if st.button('Predict Business Success'):
 
-        data = pd.DataFrame({
+        areas_name = [
+        'Philadelphia', 
+        'Indianapolis', 
+        'St. Louis', 
+        'Nashville', 
+        'New Orleans', 
+        'Orlando',
+        'Tucson', 
+        'Santa Barbara', 
+        'Reno', 
+        'Boise']
+
+
+        df_1 = pd.DataFrame({
             'ambience': [ambience],
             'garage': [garage],
             'credit_cards': [credit_cards],
-            'price_range': [price_range],
             'bike_parking': [bike_parking],
-            'area': [area],
-            'type_of_business': [type_of_business],
-            
-            'noise_level': [noise_level],
-            'weekends': [weekends],
-            'open_hours': [open_hours],
-            
-            'good_for_groups': [good_for_groups],
-            'good_for_kids': [good_for_kids],
-            'has_tv': [has_tv],
-            'outdoor_seating': [outdoor_seating],
-            'alcohol': [alcohol],
+            'wifi': [wifi],
             'delivery': [delivery],
-            
-            
-            
+            'good_for_kids': [good_for_kids],
+            'outdoor_seating': [outdoor_seating],
+            'reservations': [reservations],
+            'has_tv': [has_tv],
+            'good_for_groups': [good_for_groups],
+            'alcohol': [alcohol],
+            'by_appointment_only': [by_appointment_only],
             'caters': [caters],
             'elegancy': [elegancy],
-            'by_appointment_only': [by_appointment_only],
-            'wifi': [wifi]
+            'noise_level': [noise_level],
+            'meal_diversity': [meal_diversity]
         })
 
-        print(data)
+        df_1 = df_1.astype(int)
 
-        #prediction = predict(data)
+        df_2 = pd.DataFrame({   'Restaurants': [0.0],
+                                                'Food': [0.0],
+                                                'Shopping': [0.0],
+                                                'Home Services': [0.0],
+                                                'Beauty & Spas': [0.0],
+                                                'Nightlife': [0.0],
+                                                'Health & Medical': [0.0],
+                                                'Local Services': [0.0],
+                                                'Bars': [0.0],
+                                                'Automotive': [0.0]})
 
-        if 1 == 1:
+        if type_of_business == 'Restaurant':
+            df_2['Restaurants'] = 1.0
+        elif type_of_business == 'Food':
+            df_2['Food'] = 1.0
+        elif type_of_business == 'Shopping':
+            df_2['Shopping'] = 1.0
+        elif type_of_business == 'Home Services':
+            df_2['Home Services'] = 1.0
+        elif type_of_business == 'Beauty & Spas':
+            df_2['Beauty & Spas'] = 1.0
+        elif type_of_business == 'Nightlife':
+            df_2['Nightlife'] = 1.0
+        elif type_of_business == 'Health & Medical':
+            df_2['Health & Medical'] = 1.0
+        elif type_of_business == 'Local Services':
+            df_2['Local Services'] = 1.0
+        elif type_of_business == 'Bars':
+            df_2['Bars'] = 1.0
+        elif type_of_business == 'Automotive':
+            df_2['Automotive'] = 1.0
+
+        df_3 = pd.DataFrame({'weekends': [int(weekends)],
+                            'open_hours': [float(open_hours)]})
+
+
+        df_4 = pd.DataFrame({   'areas_0': [0.0],
+                                    'areas_1': [0.0],
+                                    'areas_2': [0.0],
+                                    'areas_3': [0.0],
+                                    'areas_4': [0.0],
+                                    'areas_5': [0.0],
+                                    'areas_6': [0.0],
+                                    'areas_8': [0.0],
+                                    'areas_9': [0.0],
+                                    'areas_10': [0.0]})
+
+        if area == 'Philadelphia':
+            df_4['areas_0'] = 1.0
+        elif area == 'Indianapolis':
+            df_4['areas_1'] = 1.0
+        elif area == 'St. Louis':    
+            df_4['areas_2'] = 1.0
+        elif area == 'Nashville':
+            df_4['areas_3'] = 1.0
+        elif area == 'New Orleans':
+            df_4['areas_4'] = 1.0
+        elif area == 'Orlando':
+            df_4['areas_5'] = 1.0
+        elif area == 'Tucson':
+            df_4['areas_6'] = 1.0
+        elif area == 'Santa Barbara':
+            df_4['areas_8'] = 1.0
+        elif area == 'Reno':
+            df_4['areas_9'] = 1.0
+        elif area == 'Boise':
+            df_4['areas_10'] = 1.0
+
+        df_5 = pd.DataFrame({   'price_ranges_0': [0.0],
+                                            'price_ranges_1': [0.0],
+                                            'price_ranges_2': [0.0],
+                                            'price_ranges_3': [0.0],
+                                            'price_ranges_4': [0.0]})
+        if price_range == 0:
+            df_5['price_ranges_0'] = 1.0
+        elif price_range == 1:
+            df_5['price_ranges_1'] = 1.0
+        elif price_range == 2:
+            df_5['price_ranges_2'] = 1.0
+        elif price_range == 3:
+            df_5['price_ranges_3'] = 1.0
+        elif price_range == 4:
+            df_5['price_ranges_4'] = 1.0
+
+        df_predictions_final = pd.concat([df_1, df_2, df_3, df_4, df_5], axis = 1)
+        
+        clf = pickle.load(open('./model/xgb_business.pkl', 'rb'))
+
+        df_predictions_final.columns = clf.get_booster().feature_names
+
+        print(df_predictions_final)
+        
+        prediction = clf.get_booster().predict(xgboost.DMatrix(df_predictions_final))
+        
+        st.success('Business probability of success: {prediction:.2f}'.format(prediction = prediction[0]))
+
+        if prediction > 0.5:
             st.success('Business is popular')
         else:
             st.error('Business is not popular')
