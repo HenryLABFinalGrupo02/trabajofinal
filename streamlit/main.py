770986a2cbb10f165ac4430effe47fafff94c7d5
dav <acidminded95@gmail.com>
2022-12-01 19:30:31 -0600

    working dashboard with curated business list

--- streamlit/main.py
+++ streamlit/main.py
@@ -17,15 +17,6 @@ st.set_page_config(
 with open('style.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
-##################
-## IMPORT DATA ###
-##################
-
-business = pd.read_csv(r'pages/main/data/business_1000.csv')
-checkin = pd.read_csv(r'pages/main/data/checkin_1000.csv')
-review = pd.read_csv(r'pages/main/data/review_1000.csv')
-#tip = pd.read_csv(r'C:\Users\USER\Documents\SOYHENRY\LABS\TRABAJO_GRUPAL\trabajofinal\Airflow-Spark\data\tip_1000.csv')
-#user = pd.read_csv(r'C:\Users\USER\Documents\SOYHENRY\LABS\TRABAJO_GRUPAL\trabajofinal\Airflow-Spark\data\user_1000.csv')
 
 ##################
 ###### MENU ######
